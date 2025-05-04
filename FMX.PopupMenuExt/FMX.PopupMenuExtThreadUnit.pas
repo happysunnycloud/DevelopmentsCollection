14f5124f02d20892da08e7458ba6cee747c36b95
AnnihilatoR <AnnihilatoR@localhost>
2025-05-04 16:51:23 +0300

    переделка popupmenu

---
+++ FMX.PopupMenuExt/FMX.PopupMenuExtThreadUnit.pas
@@ -0,0 +1,177 @@
+﻿unit FMX.PopupMenuExtThreadUnit;
+
+interface
+
+uses
+    System.Classes
+  , System.SyncObjs
+  , System.SysUtils
+  , FMX.FormExtUnit
+  ;
+
+type
+  TStepDirection = (sdForward = 0, sdBackward = 1);
+
+  TPopupMenuExtThread = class(TThread)
+  strict private
+    FCriticalSection: TCriticalSection;
+    FForm: TFormExt;
+    FStepDirection: TStepDirection;
+    FDoneEvent: TEvent;
+
+    FTimeIsOutFixed: Boolean;
+    FClickFixed: Boolean;
+    FClickedItem: TObject;
+
+    FFormOwner: TFormExt;
+
+    procedure SetClickedItem(const AClickedItem: TObject);
+    function GetClickedItem: TObject;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(
+      const AForm: TFormExt;
+      const AStepDirection: TStepDirection;
+      const ASuspended: Boolean);
+    destructor Destroy; override;
+
+    procedure WaitForDone;
+
+    property TimeIsOutFixed: Boolean read FTimeIsOutFixed;
+    property ClickFixed: Boolean read FClickFixed;
+    property Form: TFormExt read FForm;
+    property ClickedItem: TObject read GetClickedItem write SetClickedItem;
+    property FormOwner: TFormExt read FFormOwner write FFormOwner;
+  end;
+
+implementation
+
+uses
+    System.Types
+  , Winapi.Windows
+  ;
+
+{ TPopupMenuExtThread }
+
+constructor TPopupMenuExtThread.Create(
+  const AForm: TFormExt;
+  const AStepDirection: TStepDirection;
+  const ASuspended: Boolean);
+begin
+  FCriticalSection := TCriticalSection.Create;
+  FForm := AForm;
+  FStepDirection := AStepDirection;
+  FDoneEvent := TEvent.Create(nil, true, false, '', false);
+  FTimeIsOutFixed := false;
+  FClickFixed := false;
+  FClickedItem := nil;
+  FFormOwner := nil;
+
+  inherited Create(ASuspended);
+end;
+
+destructor TPopupMenuExtThread.Destroy;
+begin
+  FreeAndNil(FDoneEvent);
+  FreeAndNil(FCriticalSection);
+end;
+
+procedure TPopupMenuExtThread.WaitForDone;
+begin
+  while FDoneEvent.WaitFor(300) <> wrSignaled do
+  begin
+  end;
+end;
+
+procedure TPopupMenuExtThread.SetClickedItem(const AClickedItem: TObject);
+begin
+  FCriticalSection.Enter;
+  try
+    FClickedItem := AClickedItem;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+function TPopupMenuExtThread.GetClickedItem: TObject;
+begin
+  FCriticalSection.Enter;
+  try
+    Result := FClickedItem;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+procedure TPopupMenuExtThread.Execute;
+
+  function _IsMouseOverForm(const AForm: TFormExt): Boolean;
+  var
+    Point: TPoint;
+    RectF: TRectF;
+  begin
+    Result := false;
+
+    if AForm = nil then
+      Exit;
+
+    GetCursorPos(Point);
+
+    RectF  := TRectF.Create(AForm.ClientToScreen(AForm.ClientRect.TopLeft),
+                            AForm.ClientToScreen(AForm.ClientRect.BottomRight));
+
+    if not RectF.IsEmpty then
+      if RectF.Contains(Point) then
+      begin
+        Result := true;
+      end;
+  end;
+
+var
+  i: Integer;
+  CountDown: Integer;
+begin
+  FDoneEvent.ResetEvent;
+
+  if FStepDirection = sdForward then
+  begin
+    CountDown := 600;
+    Sleep(400);
+  end
+  else
+    CountDown := 200;
+
+  while not Terminated and not Assigned(ClickedItem) do
+  begin
+    if not _IsMouseOverForm(FForm) then
+    begin
+      i := CountDown;
+      while not Terminated and not _IsMouseOverForm(FForm) and not Assigned(ClickedItem) do
+      begin
+        Sleep(100);
+
+        Dec(i, 100);
+
+        if i < 0 then
+        begin
+          Terminate;
+
+          FTimeIsOutFixed := true;
+        end;
+      end;
+    end
+    else
+    begin
+      Sleep(100);
+    end;
+  end;
+
+  if Assigned(ClickedItem) then
+    FClickFixed := true;
+
+  FDoneEvent.SetEvent;
+end;
+
+end.
+
---
+++ FMX.PopupMenuExt/FMX.PopupMenuExtUnit.pas
@@ -0,0 +1,727 @@
+﻿unit FMX.PopupMenuExtUnit;
+
+interface
+
+uses
+    System.Generics.Collections
+  , System.Classes
+  , System.SyncObjs
+  , FMX.FormExtUnit
+  , FMX.PopupMenuExtThreadUnit
+  ;
+
+const
+  PARENT_ARROW = '>>';
+
+type
+  TItem = class;
+
+  TItems = class(TList<TItem>)
+  public
+    procedure GetItemsByParent(const AParent: TItem; const AItems: TItems);
+  end;
+
+  TItem = class
+  strict private
+    FParent: TItem;
+    FChildren: TItems;
+    FText: String;
+    FOnClick: TNotifyEvent;
+    FFormOwner: TFormExt;
+
+    procedure SetParent(const AParent: TItem);
+    function GetLevel: Word;
+  private
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    property Parent: TItem read FParent write SetParent;
+    property Children: TItems read FChildren write FChildren;
+    property Text: String read FText write FText;
+    property OnClick: TNotifyEvent read FOnClick  write FOnClick;
+    property FormOwner: TFormExt read FFormOwner write FFormOwner;
+    property Level: Word read GetLevel;
+  end;
+
+  TPopupMenuExt = class(TComponent)
+  strict private
+    FItems: TItems;
+    FPopupForm: TFormExt;
+    FPopupMenuThread: TPopupMenuExtThread;
+    FDoneEvent: TEvent;
+    /// <summary>
+    ///   Выставляется в случае закрытия всего приложения
+    ///   При выставленном флаге, сворачиваем работу меню
+    /// </summary>
+    FToDoClose: Boolean;
+
+    function FindOpenedForm(const AItem: TItem): TFormExt;
+
+    procedure OnItemMouseEnterHandler(Sender: TObject);
+    procedure OnItemMouseLeaveHandler(Sender: TObject);
+    procedure OnItemClickHandler(Sender: TObject);
+
+    procedure OnTerminatePopupMenuThreadHandler(Sender: TObject);
+
+    procedure TimeIsOutFixed(const AForm: TFormExt);
+    procedure ItemClickFixed(const ASender: TObject);
+
+    procedure StartPopupMenuThread(
+      const AForm: TFormExt;
+      const AStepDirection: TStepDirection);
+  private
+    procedure OnTimerHandler(Sender: TObject);
+  public
+    constructor Create; reintroduce;
+    destructor Destroy; override;
+
+    procedure Add(const AItem: TItem);
+    procedure Open(const X, Y: Single; const AParentItem: TItem = nil);
+    procedure Close;
+
+    property Items: TItems read FItems;
+  end;
+
+implementation
+
+uses
+    System.SysUtils
+  , System.UITypes
+  , Winapi.Windows
+  , Winapi.ShellAPI
+  , FMX.Graphics
+  , FMX.Layouts
+  , FMX.Types
+  , FMX.Objects
+  , FMX.Forms
+  , FMX.Controls
+  , FMX.Platform.Win
+  ;
+
+type
+  TFormExtHelper = class helper for TFormExt
+  private
+    procedure SetParentItem(const AParentItem: TItem);
+    function GetParentItem: TItem;
+  public
+    property ParentItem: TItem read GetParentItem write SetParentItem;
+  end;
+
+  TLayoutHelper = class helper for TLayout
+  private
+    procedure SetParentItem(const AParentItem: TItem);
+    function GetParentItem: TItem;
+  public
+    property ItemOwner: TItem read GetParentItem write SetParentItem;
+  end;
+
+// Находит положение панели задач
+// ARect - координаты, результат - положение
+function FindTaskBarPos(var ARect: TRect; var AAutoHide: Boolean): Integer;
+var
+  AppData: TAppBarData;
+begin
+  AppData.Hwnd := FindWindowW('Shell_TrayWnd', nil);
+  if AppData.Hwnd = 0 then
+    RaiseLastOSError;
+    //RaiseLastWin32Error;
+  AppData.cbSize := SizeOf(TAppBarData);
+  if SHAppBarMessage(ABM_GETTASKBARPOS, AppData) = 0 then
+    raise Exception.Create('SHAppBarMessage runtime error for requesting Taskbar');
+  Result := AppData.uEdge;
+  ARect := AppData.rc;
+  AAutoHide := (SHAppBarMessage(ABM_GETSTATE, AppData) and ABS_AUTOHIDE) <> 0;
+end;
+
+procedure TaskBarPositionDelta(const AForm: TFormExt);
+
+  function OverlapRects(const R0, R1: TRect): Boolean;
+  var
+    Temp: TRect;
+  begin
+    Result := False;
+    if not UnionRect(Temp, R0, R1) then
+      Exit;
+    if (Temp.Right - Temp.Left <= R0.Right - R0.Left + R1.Right - R1.Left) and
+       (Temp.Bottom - Temp.Top <= R0.Bottom - R0.Top + R1.Bottom - R1.Top)
+    then
+      Result := True;
+  end;
+
+var
+  TaskBarRect:     TRect;
+  TaskbarAutoHide: Boolean;
+  TaskBarPos:      Integer;
+  X, Y:            Integer;
+  R0, R1:          TRect;
+begin
+  TaskBarPos := FindTaskBarPos(TaskBarRect, TaskbarAutoHide);
+
+  R0 := AForm.Bounds;
+  R1 := TaskBarRect;
+
+  X := AForm.Left;
+  Y := AForm.Top;
+  if OverlapRects(R0, R1) then
+  begin
+    case TaskBarPos of
+      ABE_BOTTOM:
+      begin
+        AForm.Left := X - AForm.Width;
+
+        if Y + AForm.Height > TaskBarRect.TopLeft.Y then
+          AForm.Top := TaskBarRect.TopLeft.Y - AForm.Height
+        else
+          AForm.Top := Y;
+      end;
+      ABE_LEFT:
+      begin
+        if X < TaskBarRect.BottomRight.X then
+          AForm.Left := TaskBarRect.BottomRight.X
+        else
+          AForm.Left := X;
+
+        if Y + AForm.Height > TaskBarRect.BottomRight.Y then
+          AForm.Top := TaskBarRect.BottomRight.Y - AForm.Height
+        else
+          AForm.Top := Y;
+      end;
+      ABE_RIGHT:
+      begin
+        if X > TaskBarRect.TopLeft.X then
+          AForm.Left := TaskBarRect.TopLeft.X - AForm.Width
+        else
+          AForm.Left := X - AForm.Width;
+
+        if Y + AForm.Height > TaskBarRect.BottomRight.Y then
+          AForm.Top := TaskBarRect.BottomRight.Y - AForm.Height
+        else
+          AForm.Top := Y;
+      end;
+      ABE_TOP:
+      begin
+        AForm.Left := X - AForm.Width;
+
+        if Y < TaskBarRect.BottomRight.Y then
+          AForm.Top := TaskBarRect.BottomRight.Y
+        else
+          AForm.Top := Y;
+      end;
+    end;
+  end;
+end;
+
+{ TFormExtHelper }
+
+procedure TFormExtHelper.SetParentItem(const AParentItem: TItem);
+begin
+  Self.TagObject := AParentItem;
+end;
+
+function TFormExtHelper.GetParentItem: TItem;
+begin
+  Result := TItem(Self.TagObject);
+end;
+
+{ TLayoutHelper }
+
+procedure TLayoutHelper.SetParentItem(const AParentItem: TItem);
+begin
+  Self.TagObject := AParentItem;
+end;
+
+function TLayoutHelper.GetParentItem: TItem;
+begin
+  Result := TItem(Self.TagObject);
+end;
+
+{ TItems }
+
+procedure TItems.GetItemsByParent(const AParent: TItem; const AItems: TItems);
+var
+  Item: TItem;
+begin
+  AItems.Clear;
+  if not Assigned(AParent) then
+  begin
+    for Item in Self do
+    begin
+      if not Assigned(Item.Parent) then
+        AItems.Add(Item);
+    end;
+  end
+  else
+  begin
+    for Item in AParent.Children do
+    begin
+      AItems.Add(Item);
+    end;
+  end;
+end;
+
+{ TItem }
+
+constructor TItem.Create;
+begin
+  FParent := nil;
+  FChildren := TItems.Create;
+  FText := '';
+  FOnClick := nil;
+  FFormOwner := nil;
+end;
+
+destructor TItem.Destroy;
+begin
+  FreeAndNil(FChildren);
+end;
+
+procedure TItem.SetParent(const AParent: TItem);
+begin
+  FParent := AParent;
+  AParent.Children.Add(Self);
+end;
+
+function TItem.GetLevel: Word;
+var
+  Level: Word;
+  Item: TItem;
+begin
+  Level := 0;
+
+  Item := Self;
+  while Assigned(Item.Parent) do
+  begin
+    Inc(Level);
+
+    Item := Item.Parent;
+  end;
+
+  Result := Level;
+end;
+
+{ TPopupMenuExt }
+
+function TPopupMenuExt.FindOpenedForm(const AItem: TItem): TFormExt;
+var
+  i: Word;
+  Form: TFormExt;
+begin
+  Result := nil;
+
+  i := ComponentCount;
+  while i > 0 do
+  begin
+    Dec(i);
+
+    if Components[i] is TFormExt then
+    begin
+      Form := TFormExt(Components[i]);
+      if Form.ParentItem = AItem then
+        Exit(Form);
+    end;
+  end;
+end;
+
+procedure TPopupMenuExt.OnItemMouseEnterHandler(Sender: TObject);
+var
+  Rectangle: TRectangle;
+begin
+  Rectangle := TRectangle(TLayout(Sender).Children[0]);
+  Rectangle.Fill.Color := TAlphaColorRec.Cornflowerblue;
+end;
+
+procedure TPopupMenuExt.OnItemMouseLeaveHandler(Sender: TObject);
+var
+  Rectangle: TRectangle;
+begin
+  Rectangle := TRectangle(TLayout(Sender).Children[0]);
+  Rectangle.Fill.Color := $FFE0E0E0;
+end;
+
+procedure TPopupMenuExt.OnItemClickHandler(Sender: TObject);
+var
+  Layout: TLayout;
+  Item: TItem;
+begin
+  Layout := TLayout(Sender);
+  Item := Layout.ItemOwner;
+  if Assigned(FPopupMenuThread) then
+    FPopupMenuThread.ClickedItem := Item;
+end;
+
+procedure TPopupMenuExt.OnTerminatePopupMenuThreadHandler(Sender: TObject);
+var
+  PopupMenuThread: TPopupMenuExtThread;
+begin
+  PopupMenuThread := FPopupMenuThread;
+  FPopupMenuThread := nil;
+
+  if FToDoClose then
+    Exit;
+
+  if PopupMenuThread.TimeIsOutFixed then
+    TimeIsOutFixed(PopupMenuThread.Form)
+  else
+  if PopupMenuThread.ClickFixed then
+    ItemClickFixed(PopupMenuThread.ClickedItem);
+end;
+
+procedure TPopupMenuExt.TimeIsOutFixed(const AForm: TFormExt);
+var
+  ParentItem: TItem;
+  ParentForm: TFormExt;
+begin
+  ParentItem := AForm.ParentItem;
+
+  AForm.Free;
+
+  if FToDoClose then
+    Exit;
+
+  if not Assigned(ParentItem) then
+  begin
+    FDoneEvent.SetEvent;
+
+    Exit
+  end
+  else
+    ParentForm := ParentItem.FormOwner;
+
+  FPopupMenuThread :=  TPopupMenuExtThread.Create(ParentForm, sdBackward, true);
+  FPopupMenuThread.FreeOnTerminate := true;
+  FPopupMenuThread.OnTerminate := OnTerminatePopupMenuThreadHandler;
+  FPopupMenuThread.Start;
+end;
+
+procedure TPopupMenuExt.ItemClickFixed(const ASender: TObject);
+
+  procedure CloseSameLevelForms(const AItemOwner: TItem);
+  var
+    Level: Word;
+    ParentItemForm: TFormExt;
+    Form: TFormExt;
+    i: Word;
+  begin
+    ParentItemForm := FindOpenedForm(AItemOwner);
+    Level := AItemOwner.Level;
+
+    i := ComponentCount;
+    while i > 0 do
+    begin
+      Dec(i);
+
+      if Components[i] is TFormExt then
+      begin
+        Form := TFormExt(Components[i]);
+        if not Assigned(Form.ParentItem) then
+          Exit;
+
+        if Form.ParentItem.Level >= Level then
+          if Form <> ParentItemForm then
+            Form.Free;
+      end;
+    end;
+  end;
+
+var
+  ItemOwner: TItem;
+  Point: TPoint;
+  OpenedForm: TFormExt;
+begin
+  GetCursorPos(Point);
+
+  ItemOwner := TItem(ASender);
+  CloseSameLevelForms(ItemOwner);
+
+  OpenedForm := FindOpenedForm(ItemOwner);
+  if Assigned(OpenedForm) then
+  begin
+    SetForegroundWindow(FmxHandleToHWND(OpenedForm.Handle));
+    StartPopupMenuThread(OpenedForm, sdForward)
+  end
+  else
+  begin
+    if ItemOwner.Children.Count = 0 then
+    begin
+      if Assigned(ItemOwner.OnClick) then
+        ItemOwner.OnClick(ItemOwner);
+      Close;
+    end
+    else
+      Open(Point.X, Point.Y, ItemOwner);
+  end;
+end;
+
+procedure TPopupMenuExt.OnTimerHandler(Sender: TObject);
+var
+ i:integer;
+ p:TPoint;
+ myCursorPos: TPoint;
+ Control: TControl;
+begin
+ if FPopupForm = nil then
+  Exit;
+ OutputDebugString('Start');
+ GetCursorPos(myCursorPos);
+ for i:=0 to FPopupForm.ComponentCount-1 do
+ begin
+  if not (FPopupForm.Components[i] is TControl) then Continue;
+  Control := TControl(FPopupForm.Components[i]);
+  p := FPopupForm.ScreenToClient(myCursorPos).Round;
+//  if PtInRect(Control.BoundsRect.Round, p) then
+//  if
+//    (myCursorPos.X>=p.X)and(myCursorPos.Y>=p.Y) and
+//    (myCursorPos.X<=(p.X+Width))and(myCursorPos.Y<=(p.Y+Height)) then
+  begin
+    OutputDebugString(pwidechar('ClassName -> ' + Control.ClassName + '|' + 'Name -> ' + Control.Name));
+  end
+ end;
+ OutputDebugString('Finish');
+end;
+
+constructor TPopupMenuExt.Create;
+var
+  Timer: TTimer;
+begin
+  FItems := TItems.Create;
+  FPopupForm := nil;
+  FPopupMenuThread := nil;
+  FDoneEvent := TEvent.Create(nil, true, false, '', false);
+  FToDoClose := false;
+
+  Timer := TTimer.Create(Self);
+  Timer.Interval := 1000;
+  Timer.Enabled := true;
+  Timer.OnTimer := OnTimerHandler;
+end;
+
+destructor TPopupMenuExt.Destroy;
+var
+  i: Word;
+begin
+  i := FItems.Count;
+  while i > 0 do
+  begin
+    Dec(i);
+
+    FItems[i].Free;
+  end;
+
+  FreeAndNil(FItems);
+  FreeAndNil(FDoneEvent);
+
+  inherited;
+end;
+
+procedure TPopupMenuExt.Add(const AItem: TItem);
+begin
+  FItems.Add(AItem);
+end;
+
+procedure TPopupMenuExt.Close;
+var
+  Form: TFormExt;
+  i: Word;
+begin
+  if Assigned(FPopupMenuThread) then
+  begin
+    FToDoClose := true;
+
+    FPopupMenuThread.Terminate;
+    FPopupMenuThread.WaitForDone;
+  end;
+
+  i := ComponentCount;
+  while i > 0 do
+  begin
+    Dec(i);
+
+    if Components[i] is TFormExt then
+    begin
+      Form := TFormExt(Components[i]);
+      RemoveComponent(Form);
+      Form.Free;
+    end;
+  end;
+end;
+
+procedure TPopupMenuExt.Open(
+  const X, Y: Single;
+  const AParentItem: TItem = nil);
+
+  function _GetMaxTextWidth(const AText: TText; const AItems: TItems): Single;
+  var
+    Items:        TItems;
+    MaxTextWidth: Single;
+    TextWidth:    Single;
+    i:            Word;
+  begin
+    Items := AItems;
+
+    MaxTextWidth := 0;
+    i := 0;
+    while i < Items.Count do
+    begin
+      TextWidth := AText.Canvas.TextWidth(Items[i].Text);
+      if MaxTextWidth < TextWidth then
+        MaxTextWidth := TextWidth;
+      Inc(i);
+    end;
+
+    Result := MaxTextWidth;
+  end;
+
+var
+  Item: TItem;
+  ItemCount: Word;
+  Layout: TLayout;
+  BackgroundRectangle: TRectangle;
+  Rectangle: TRectangle;
+  Text: TText;
+  TextArrow: TText;
+  ParentArrowWidth: Single;
+  ItemsByParent: TItems;
+  MaxTextWidth: Single;
+  ItemHeight: Word;
+  PopupFormWidth: Integer;
+  OpenedForm: TFormExt;
+begin
+  if not Assigned(AParentItem) then
+  begin
+    OpenedForm := FindOpenedForm(nil);
+    if Assigned(OpenedForm) then
+    begin
+      OpenedForm.Left := Trunc(X);
+      OpenedForm.Top := Trunc(Y);
+      SetForegroundWindow(FmxHandleToHWND(OpenedForm.Handle));
+
+      Exit;
+    end;
+  end;
+
+  FDoneEvent.ResetEvent;
+
+  FPopupForm := TFormExt.CreateNew(Self);
+  FPopupForm.BorderStyle := TFmxFormBorderStyle.None;
+  FPopupForm.Left := Trunc(X);
+  FPopupForm.Top := Trunc(Y);
+  FPopupForm.Height := 100;
+  FPopupForm.Show;
+
+  PopupFormWidth := 0;
+  ItemHeight := 30;
+  ItemCount := 0;
+
+  ItemsByParent := TItems.Create;
+  FItems.GetItemsByParent(AParentItem, ItemsByParent);
+  try
+    BackgroundRectangle := TRectangle.Create(FPopupForm);
+    BackgroundRectangle.Parent := FPopupForm;
+    BackgroundRectangle.Align := TAlignLayout.Client;
+    BackgroundRectangle.Stroke.Thickness := 0;
+    BackgroundRectangle.Stroke.Kind := TBrushKind.None;
+    BackgroundRectangle.SendToBack;
+    BackgroundRectangle.Name := 'BackgroundRectangle';
+    BackgroundRectangle.HitTest := false;
+    BackgroundRectangle.Fill.Color := TAlphaColorRec.Lightgray;
+
+    for Item in ItemsByParent do
+    begin
+      Item.FormOwner := FPopupForm;
+
+      Layout := TLayout.Create(BackgroundRectangle);
+      Layout.Parent := BackgroundRectangle;
+      Layout.ItemOwner := Item;
+      Layout.Align := TAlignLayout.Bottom;
+      Layout.Height := ItemHeight;
+      Layout.HitTest := true;
+      Layout.OnClick := OnItemClickHandler;
+      Layout.OnMouseEnter := OnItemMouseEnterHandler;
+      Layout.OnMouseLeave := OnItemMouseLeaveHandler;
+
+      Rectangle := TRectangle.Create(Layout);
+      Rectangle.Parent := Layout;
+      Rectangle.Align := TAlignLayout.Client;
+      Rectangle.HitTest := false;
+      Rectangle.Stroke.Thickness := 0;
+      Rectangle.Stroke.Kind := TBrushKind.None;
+      Rectangle.Margins.Top := 2;
+      Rectangle.Margins.Left := 2;
+      Rectangle.Margins.Right := 2;
+      Rectangle.Margins.Bottom := 2;
+
+      TextArrow := TText.Create(Rectangle);
+      TextArrow.Parent := Rectangle;
+      TextArrow.Align := TAlignLayout.Right;
+      TextArrow.Text := PARENT_ARROW;//' ►';//Char($25BA);
+      TextArrow.HitTest := false;
+      TextArrow.TextSettings.HorzAlign := TTextAlign.Trailing;
+      TextArrow.Margins.Right := 5;
+
+      ParentArrowWidth := TextArrow.Canvas.TextWidth(TextArrow.Text);
+      if Item.Children.Count = 0 then
+      begin
+        ParentArrowWidth := 0;
+        TextArrow.Margins.Left := 0;
+        TextArrow.Margins.Right := 0;
+        TextArrow.Width := 0;
+        TextArrow.Visible := false;
+      end;
+
+      Text := TText.Create(Rectangle);
+      Text.Parent := Rectangle;
+      Text.Align := TAlignLayout.Client;
+      Text.Text := Item.Text;
+      Text.HitTest := false;
+      Text.TextSettings.HorzAlign := TTextAlign.Leading;
+      Text.Margins.Left := 5;
+
+      MaxTextWidth := _GetMaxTextWidth(Text, ItemsByParent);
+      PopupFormWidth := Trunc(
+        (
+          MaxTextWidth + ParentArrowWidth + 50
+        ) +
+        (
+          Text.Margins.Left +
+          Text.Margins.Right +
+          TextArrow.Margins.Left +
+          TextArrow.Margins.Right +
+          Rectangle.Margins.Left +
+          Rectangle.Margins.Right
+       )
+      );
+
+//      ItemHeight := Trunc(Text.Canvas.TextHeight('W'));
+
+      Layout.Align := TAlignLayout.Top;
+
+      Inc(ItemCount);
+    end;
+
+    FPopupForm.Width := PopupFormWidth;
+  finally
+    FreeAndNil(ItemsByParent);
+  end;
+
+  FPopupForm.Height := ItemCount * ItemHeight;
+  FPopupForm.ParentItem := AParentItem;
+  TaskBarPositionDelta(FPopupForm);
+  SetForegroundWindow(FmxHandleToHWND(FPopupForm.Handle));
+
+  StartPopupMenuThread(FPopupForm, sdForward);
+end;
+
+procedure TPopupMenuExt.StartPopupMenuThread(
+  const AForm: TFormExt;
+  const AStepDirection: TStepDirection);
+begin
+  FPopupMenuThread := TPopupMenuExtThread.Create(AForm, AStepDirection, true);
+  FPopupMenuThread.FreeOnTerminate := true;
+  FPopupMenuThread.OnTerminate := OnTerminatePopupMenuThreadHandler;
+  FPopupMenuThread.Start;
+  FPopupMenuThread.FormOwner := FPopupForm;
+end;
+
+end.
+
