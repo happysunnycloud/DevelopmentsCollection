9ae3bd52256cbebb755f965ca6f0cabb4df16b7a
AnnihilatoR <AnnihilatoR@localhost>
2025-12-01 01:12:19 +0300

    добавляем экстрактов текстов из упакованного файла

---
+++ FilePacker/TextExtractorUnit.pas
@@ -0,0 +1,55 @@
+﻿// Класс извлечения текста из файла с упакованными текстами
+unit TextExtractorUnit;
+
+interface
+
+uses
+    FilePackerUnit
+  ;
+
+type
+  TTextExtractor = class(TFilePacker)
+  public
+    class function ExtractToString(
+      const AFilePacker: TFilePacker;
+      const AExtractingFileName: String): String;
+  end;
+
+implementation
+
+uses
+    System.Classes
+  , System.SysUtils
+  ;
+
+{ TTextExtractor }
+
+class function TTextExtractor.ExtractToString(
+  const AFilePacker: TFilePacker;
+  const AExtractingFileName: String): String;
+var
+  StringStream: TStringStream;
+  MemoryStream: TMemoryStream;
+begin
+  Result := '';
+
+  if not Assigned(AFilePacker) then
+    raise Exception.Create('File packer reference is nil');
+
+  StringStream := TStringStream.Create;
+  MemoryStream := TMemoryStream.Create;
+  try
+    try
+      AFilePacker.ExtractToMemoryStream(AExtractingFileName, MemoryStream);
+      StringStream.LoadFromStream(MemoryStream);
+      Result := StringStream.DataString;
+    except
+      raise;
+    end;
+  finally
+    FreeAndNil(MemoryStream);
+    FreeAndNil(StringStream);
+  end;
+end;
+
+end.
