2c3f7a14b548a9975274c9ae01758cdedd5ddb6d
AnnihilatoR <AnnihilatoR@localhost>
2025-12-25 21:22:28 +0300

    init

---
+++ SQL/BaseDBAccessUnit.pas
@@ -0,0 +1,191 @@
+﻿unit BaseDBAccessUnit;
+
+interface
+
+uses
+    System.SyncObjs
+  , DBToolsUnit
+  , SQLTemplatesUnit
+  , ParamsExtUnit
+  ;
+
+const
+  NULL_ID = 0;
+  TIME_OUT_SECONDS = 10;
+  NULL_DATETIME = 0;
+
+type
+  TDBAResultCode = (rcFault = -1, rcOk = 0); //DBA = D - data, B - base,  A - access
+
+  TParamsProcRef = reference to
+    procedure(
+      const AInParams: TParamsExt;
+      const AOutParams: TParamsExt);
+
+  TBaseDBAccess = class
+  strict private
+    FCriticalSection: TCriticalSection;
+    FDBFileName: String;
+    FSQLTemplates: TSQLTemplates;
+  protected
+    property SQLTemplates: TSQLTemplates read FSQLTemplates;
+    property DBFileName: String read FDBFileName;
+  public
+    constructor Create(
+      const ADBFileName: String;
+      const ASQLTemplatesPath: String;
+      const ATemplatesKind: TTemplatesKind);
+    destructor Destroy; override;
+
+    function DBAParamsFunc(
+      const AParamsProcRef: TParamsProcRef;
+      const AInParams: TParamsExt;
+      const AOutParams: TParamsExt): TDBAResultCode;
+  end;
+
+implementation
+
+uses
+    System.Classes
+  , System.SysUtils
+  , System.Generics.Collections
+  , FireDAC.Stan.Error
+  , FireDAC.Phys.SQLiteWrapper
+  , DBExceptionContainerUnit
+  ;
+
+constructor TBaseDBAccess.Create(
+  const ADBFileName: String;
+  const ASQLTemplatesPath: String;
+  const ATemplatesKind: TTemplatesKind);
+begin
+  FDBFileName := ADBFileName;
+  try
+    FCriticalSection := TCriticalSection.Create;
+    FSQLTemplates := TSQLTemplates.Create(ASQLTemplatesPath, ATemplatesKind);
+  except
+    raise;
+  end;
+end;
+
+destructor TBaseDBAccess.Destroy;
+begin
+  FreeAndNil(FCriticalSection);
+  FreeAndNil(FSQLTemplates);
+
+  inherited;
+end;
+
+function TBaseDBAccess.DBAParamsFunc(
+  const AParamsProcRef: TParamsProcRef;
+  const AInParams: TParamsExt;
+  const AOutParams: TParamsExt): TDBAResultCode;
+const
+  METHOD = 'TBaseDBAccessClass.DBAParamsFunc';
+  SPLITTER = ' -> ';
+
+  procedure _RaiseException(const AMessage: String);
+  var
+    _Message: String;
+  begin
+    _Message := AMessage;
+    raise Exception.Create(_Message);
+//    TThread.ForceQueue(nil,
+//      procedure
+//      begin
+//        raise Exception.Create(_Message);
+//      end
+//    );
+  end;
+
+var
+  ParamsProcRef: TParamsProcRef absolute AParamsProcRef;
+  InParams: TParamsExt;
+  OutParams: TParamsExt;
+  FDCommandExceptionKind: TFDCommandExceptionKind;
+  TimeOutCount: Integer;
+  MessageString: String;
+  BreakWhile: Boolean;
+begin
+  FCriticalSection.Enter;
+  try
+    TimeOutCount := TIME_OUT_SECONDS;
+    BreakWhile := false;
+
+    MessageString := '';
+
+    InParams := TParamsExt.Create;
+    OutParams := TParamsExt.Create;
+    try
+      if Assigned(AInParams) then
+        InParams.CopyFrom(AInParams);
+
+      while not BreakWhile do
+      begin
+        try
+          ParamsProcRef(InParams, OutParams);
+        except
+          on e: TDBExceptionContainer do
+          begin
+            if e.ExceptionClass = EFDDBEngineException then
+            begin
+              FDCommandExceptionKind := e.Kind;
+              if FDCommandExceptionKind = ekRecordLocked then
+              begin
+                Dec(TimeOutCount);
+
+                if TimeOutCount > 0 then
+                begin
+                  Sleep(1000);
+
+                  Continue;
+                end;
+              end;
+              MessageString :=
+                Concat(
+                  METHOD, SPLITTER,
+                  e._MethodName, SPLITTER,
+                  e.ExceptionClass.ClassName, SPLITTER,
+                  FDCommandExceptionKind.ToString, SPLITTER,
+                  e.Message);
+            end
+            else
+              MessageString :=
+                Concat(
+                  METHOD,
+                  SPLITTER,
+                  e._MethodName,
+                  SPLITTER,
+                  e.ExceptionClass.ClassName, SPLITTER,
+                  e.Message);
+          end;
+          on e: Exception do
+            MessageString := Concat(METHOD, SPLITTER, e.ClassName, SPLITTER, e.Message);
+          else
+            MessageString := Concat(METHOD, SPLITTER, 'Unknown exception');
+        end;
+
+        BreakWhile := true;
+      end;
+
+      if MessageString.Length > 0 then
+        _RaiseException(MessageString);
+
+      if Assigned(AOutParams) then
+      begin
+        AOutParams.Clear;
+        AOutParams.CopyFrom(OutParams);
+      end;
+    finally
+      FreeAndNil(InParams);
+      FreeAndNil(OutParams);
+    end;
+  finally
+    FCriticalSection.Leave;
+  end;
+
+	Result := rcOk;
+end;
+
+end.
+
---
+++ SQL/DBExceptionContainerUnit.pas
@@ -0,0 +1,158 @@
+﻿// Контейнер позволяет отслеживать в каком именно методе произошло исключение
+
+unit DBExceptionContainerUnit;
+
+interface
+
+uses
+    System.SysUtils
+  , FireDAC.Stan.Error
+  ;
+type
+  TFDCommandExceptionKindHelper = record helper for TFDCommandExceptionKind
+  public
+    function ToString: String;
+  end;
+
+  TDBExceptionContainer = class(Exception)
+  strict private
+    FExceptionClass: TClass;
+    FExceptionKind: TFDCommandExceptionKind;
+    FExceptionKindExists: Boolean;
+    FMethodName: String;
+//    FMessage: String;
+
+    procedure InitException(
+      const AExceptionClass: TClass;
+      const AMethodName: String;
+      const AMessage: String);
+  public
+    constructor Create(
+      const AExceptionClass: TClass;
+      const AExceptionKind: TFDCommandExceptionKind;
+      const AMethodName: String;
+      const AMessage: String); {reintroduce;} overload;
+    constructor Create(
+      const AExceptionClass: TClass;
+      const AMethodName: String;
+      const AMessage: String); {reintroduce;} overload;
+
+    property ExceptionClass: TClass read FExceptionClass;
+    property Kind: TFDCommandExceptionKind read FExceptionKind;
+    property _MethodName: String read FMethodName;
+//    property _Message: String read FMessage;
+    // Если FExceptionKindExists = False,
+    // Тогда FExceptionKind = ekOther
+    property ExceptionKindExists: Boolean read FExceptionKindExists;
+
+    class function CreateExceptionContainer(
+      const AE: Pointer;
+      const AMethodName: String): TDBExceptionContainer;
+  end;
+
+implementation
+
+uses
+    FireDAC.Phys.SQLiteWrapper
+  ;
+
+function TFDCommandExceptionKindHelper.ToString: String;
+begin
+  case Self of
+    ekOther: Result := 'ekOther';
+    ekNoDataFound: Result := 'ekNoDataFound';
+    ekTooManyRows: Result := 'ekTooManyRows';
+    ekRecordLocked: Result := 'ekRecordLocked';
+    ekUKViolated: Result := 'ekUKViolated';
+    ekFKViolated: Result := 'ekFKViolated';
+    ekObjNotExists: Result := 'ekObjNotExists';
+    ekUserPwdInvalid: Result := 'ekUserPwdInvalid';
+    ekUserPwdExpired: Result := 'ekUserPwdExpired';
+    ekUserPwdWillExpire: Result := 'ekUserPwdWillExpire';
+    ekCmdAborted: Result := 'ekCmdAborted';
+    ekServerGone: Result := 'ekServerGone';
+    ekServerOutput: Result := 'ekServerOutput';
+    ekArrExecMalfunc: Result := 'ekArrExecMalfunc';
+    ekInvalidParams: Result := 'ekInvalidParams'
+  else
+    Result := 'ekUnknown';
+  end;
+end;
+
+procedure TDBExceptionContainer.InitException(
+  const AExceptionClass: TClass;
+  const AMethodName: String;
+  const AMessage: String);
+begin
+  FExceptionClass := AExceptionClass;
+  FExceptionKind := ekOther;
+
+  FExceptionKindExists := false;
+  FMethodName := AMethodName;
+
+//  FMessage := AMessage;
+
+  Message := AMessage;
+end;
+
+constructor TDBExceptionContainer.Create(
+  const AExceptionClass: TClass;
+  const AExceptionKind: TFDCommandExceptionKind;
+  const AMethodName: String;
+  const AMessage: String);
+begin
+  InitException(
+    AExceptionClass,
+    AMethodName,
+    AMessage
+  );
+
+  FExceptionKind := AExceptionKind;
+  FExceptionKindExists := true;
+end;
+
+constructor TDBExceptionContainer.Create(
+  const AExceptionClass: TClass;
+  const AMethodName: String;
+  const AMessage: String);
+begin
+  InitException(
+    AExceptionClass,
+    AMethodName,
+    AMessage
+  );
+
+  FExceptionKind := ekOther;
+  FExceptionKindExists := false;
+end;
+
+class function TDBExceptionContainer.CreateExceptionContainer(
+  const AE: Pointer;
+  const AMethodName: String): TDBExceptionContainer;
+var
+  _Exception: Exception;
+begin
+  // AE передавать в другие методы нельзя,
+  // этот объект уничтожается после обработки вышестоящего исключения
+  // здесь мы достаем из AE все что нужно и забываем о нем
+
+  _Exception := Exception(AE);
+
+  if _Exception is EFDDBEngineException then
+  begin
+    Result := TDBExceptionContainer.Create(
+      Pointer(EFDDBEngineException),
+      EFDDBEngineException(_Exception).Kind,
+      AMethodName,
+      _Exception.Message)
+  end
+  else
+  begin
+    Result := TDBExceptionContainer.Create(
+      _Exception.ClassType,
+      AMethodName,
+     _Exception.Message);
+  end;
+end;
+
+end.
