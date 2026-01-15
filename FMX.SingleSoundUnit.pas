7e17449fae5d366b5280614b2e0a22ae633b0541
AnnihilatoR <AnnihilatoR@localhost>
2026-01-15 17:18:20 +0300

    теперь используемый в компоненте трид, должен обязательно подключаться к фабрике тридов

--- FMX.SingleSoundUnit.pas
+++ FMX.SingleSoundUnit.pas
@@ -1,13 +1,27 @@
 ﻿unit FMX.SingleSoundUnit;
 
+// Устанавливать значения и состояния можно только в главном потоке
+// Читать можно асинхронно из дочерних потоков
+// Читаются кэшированные данные
+
 interface
 
 uses
     System.SyncObjs
+  , System.Classes
+  , System.SysUtils
   , FMX.Media
+  , ThreadFactoryUnit
   ;
 
 type
+  TDataProc = reference to procedure (
+    const AFileName: String;
+    const ACurrentTime: TMediaTime;
+    const ADuration: TMediaTime;
+    const AVolume: Single;
+    const AState: TMediaState);
+
   TError = record
     ErrorCode:      Integer;
     ErrorText:      String;
@@ -21,13 +35,34 @@ type
     class var FileNotExists:      TError;
   end;
 
+  TSingleSoundThread = class;
+
   TSingleSound = class
   strict private
     FCriticalSection: TCriticalSection;
+
     FMediaPlayer: TMediaPlayer;
+    FSingleSoundThread: TSingleSoundThread;
+
     FLastCurrentTime: TMediaTime;
+
+    FCachedCurrentTime: TMediaTime;
+    FCachedMediaState: TMediaState;
+
+    FOnGetDataExternalHandler: TDataProc;
+    FOnFinishedExternalHandler: TProc;
+
+    // Кэшируем eдиножды FileName при загрузке трека
+    // Нет смысла вытаскивать его каджый раз в UpdateCache
+    FFileName: String;
+    // Кэшируем eдиножды Duration при загрузке трека
+    // После вызова Stop Android обнулит значение Duration
+    // По этому всегда используем кэшированное значение
+    FDuration: TMediaTime;
+    // Кэшируем eдиножды Volume при загрузке трека
+    // После вызова Stop Android обнулит значение Volume
+    // По этому всегда используем кэшированное значение
     FVolume: Single;
-    FLastVolume: Single;
 
     procedure SetFileName(const AFileName: String);
     function GetFileName: String;
@@ -35,12 +70,27 @@ type
     procedure SetCurrentTime(const ACurrentTime: TMediaTime);
     function GetCurrentTime: TMediaTime;
 
-    function GetVolume: Single;
     procedure SetVolume(const AVolume: Single);
+    function GetVolume: Single;
 
     function GetDuration: TMediaTime;
+
+    function GetMediaState: TMediaState;
+
+    procedure SaveLastValues;
+
+    procedure OnGetDataInternalHandler;
+    procedure OnFinishedInternalHandler;
+
+    procedure ResetValues;
+
+    procedure UpdateCache;
+
+    procedure RaiseMainThreadOnlyException(const AMethod: String);
+//  private
+//    property MediaPlayer: TMediaPlayer read FMediaPlayer;
   public
-    constructor Create;
+    constructor Create(const AThreadFactory: TThreadFactory);
     destructor Destroy; override;
 
     property FileName: String read GetFileName write SetFileName;
@@ -50,6 +100,10 @@ type
     // Таким образом 0.06 превращается в 0
     property Volume: Single read GetVolume write SetVolume;
     property Duration: TMediaTime read GetDuration;
+    property MediaState: TMediaState read GetMediaState;
+
+    property OnGetData: TDataProc write FOnGetDataExternalHandler;
+    property OnFinished: TProc write FOnFinishedExternalHandler;
 
     procedure Play; overload;
     procedure Play(const ACurrentTime: TMediaTime); overload;
@@ -60,76 +114,197 @@ type
     procedure UnMute;
   end;
 
-implementation
+  TSingleSoundThread = class(TThreadExt)
+  strict private
+    FCriticalSection: TCriticalSection;
+    FSingleSound: TSingleSound;
+    FHoldEvent: TEvent;
+    FDoneEvent: TEvent;
+    FGetDataEvent: TEvent;
 
-uses
-    System.SysUtils
-  ;
+    FOnGetData: TProc;
+    FOnFinished: TProc;
+
+    procedure SetOnGetData(const AProc: TProc);
+    procedure SetOnFinished(const AProc: TProc);
+
+    function GetOnGetData: TProc;
+    function GetOnFinished: TProc;
+
+    procedure OnSetTerminatedHandler(Sender: TObject);
+  protected
+    procedure InnerExecute; override;
+  public
+    constructor Create(
+      const AThreadFactory: TThreadFactory;
+      const ASingleSound: TSingleSound);
+    destructor Destroy; override;
+
+    procedure WaitForDone;
+
+    property OnGetData: TProc read GetOnGetData write SetOnGetData;
+    property OnFinished: TProc read GetOnFinished write SetOnFinished;
+
+    property GetDataEvent: TEvent read FGetDataEvent;
+    property HoldEvent: TEvent read FHoldEvent;
+  end;
+
+implementation
 
 { TSingleSound }
 
-constructor TSingleSound.Create;
+procedure TSingleSound.RaiseMainThreadOnlyException(const AMethod: String);
 begin
-  FCriticalSection := TCriticalSection.Create;
-  FMediaPlayer := TMediaPlayer.Create(nil);
-  FLastCurrentTime := 0;
-  FVolume := 0.5;
-  FMediaPlayer.Volume := FVolume;
-  FLastVolume := FVolume;
+  if TThread.CurrentThread.ThreadID <> MainThreadID then
+    raise Exception.Create(
+      Concat(AMethod, ' -> ', 'Should only be executed on the main thread'));
+end;
+
+procedure TSingleSound.UpdateCache;
+begin
+  RaiseMainThreadOnlyException('TSingleSound.UpdateCache');
+
+  FCriticalSection.Enter;
+  try
+    FCachedCurrentTime := FMediaPlayer.CurrentTime;
+    FCachedMediaState := FMediaPlayer.State;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+procedure TSingleSound.ResetValues;
+begin
+  FCriticalSection.Enter;
+  try
+    FFileName := '';
+    FCachedCurrentTime := 0;
+    FVolume := 0.5;
+    FCachedMediaState := TMediaState.Unavailable;
+
+    FLastCurrentTime := FCachedCurrentTime;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+constructor TSingleSound.Create(const AThreadFactory: TThreadFactory);
+begin
+  try
+    if not Assigned(AThreadFactory) then
+      raise Exception.Create('AThreadFactory cannot be nil');
+
+    FCriticalSection := TCriticalSection.Create;
+
+    FOnGetDataExternalHandler := nil;
+    FOnFinishedExternalHandler := nil;
+
+    FMediaPlayer := TMediaPlayer.Create(nil);
+    FSingleSoundThread := TSingleSoundThread.Create(AThreadFactory, Self);
+    FSingleSoundThread.OnGetData := OnGetDataInternalHandler;
+    FSingleSoundThread.OnFinished := OnFinishedInternalHandler;
+    FSingleSoundThread.HoldEvent.SetEvent;
+    FSingleSoundThread.Start;
+
+    ResetValues;
+  except
+    on e: Exception do
+      raise Exception.CreateFmt('TSingleSound.Create -> %s', [e.Message]);
+  end;
 end;
 
 destructor TSingleSound.Destroy;
 begin
+//  if Assigned(FSingleSoundThread) then
+//  begin
+//    FSingleSoundThread.Terminate;
+//    FSingleSoundThread.GetDataEvent.SetEvent;
+//    FSingleSoundThread.HoldEvent.SetEvent;
+//    FSingleSoundThread.WaitForDone;
+//    FSingleSoundThread := nil;
+//  end;
+
   FreeAndNil(FMediaPlayer);
+
   FreeAndNil(FCriticalSection);
-end;
-
-procedure TSingleSound.SetFileName(const AFileName: String);
-begin
-  if not FileExists(AFileName) then
-    raise Exception.CreateFmt(
-      TErrorClass.FileNotExists.ErrorTextFmt, [AFileName]);
-
-  Stop;
-
-  FCriticalSection.Enter;
-  try
-    FMediaPlayer.FileName := AFileName;
-
-  finally
-    FCriticalSection.Leave;
-  end;
 end;
 
 function TSingleSound.GetFileName: String;
 begin
   FCriticalSection.Enter;
   try
-    Result := FMediaPlayer.FileName;
+    Result := FFileName;
   finally
     FCriticalSection.Leave;
   end;
 end;
 
-procedure TSingleSound.SetCurrentTime(const ACurrentTime: TMediaTime);
+procedure TSingleSound.SetFileName(const AFileName: String);
+var
+  FileName: String;
+  Duration: TMediaTime;
 begin
+  RaiseMainThreadOnlyException('TSingleSound.SetFileName');
+
+  FileName := AFileName;
+  if not FileExists(FileName) then
+    raise Exception.CreateFmt(
+      TErrorClass.FileNotExists.ErrorTextFmt, [FileName]);
+
+  Stop;
+
+//  CurrentTime := 0;
+
+  FMediaPlayer.FileName := FileName;
+  Duration := FMediaPlayer.Duration;
+
   FCriticalSection.Enter;
   try
-    FLastCurrentTime := ACurrentTime;
-    FMediaPlayer.CurrentTime := FLastCurrentTime;
+    FFileName := FileName;
+    FDuration := Duration;
   finally
     FCriticalSection.Leave;
   end;
+
+  UpdateCache;
+end;
+
+procedure TSingleSound.SetCurrentTime(const ACurrentTime: TMediaTime);
+var
+  CurrentTime: TMediaTime;
+begin
+  RaiseMainThreadOnlyException('TSingleSound.SetCurrentTime');
+
+  CurrentTime := ACurrentTime;
+
+  FMediaPlayer.CurrentTime := CurrentTime;
+
+  UpdateCache;
+
+  SaveLastValues;
 end;
 
 function TSingleSound.GetCurrentTime: TMediaTime;
 begin
   FCriticalSection.Enter;
   try
-    Result := FMediaPlayer.CurrentTime
+    Result := FCachedCurrentTime;
   finally
     FCriticalSection.Leave;
   end;
+end;
+
+procedure TSingleSound.SetVolume(const AVolume: Single);
+begin
+  RaiseMainThreadOnlyException('TSingleSound.SetVolume');
+
+  SaveLastValues;
+
+  FVolume := AVolume;
+
+  FMediaPlayer.Volume := FVolume;
+
+  UpdateCache;
 end;
 
 function TSingleSound.GetVolume: Single;
@@ -142,89 +317,315 @@ begin
   end;
 end;
 
-procedure TSingleSound.SetVolume(const AVolume: Single);
+function TSingleSound.GetDuration: TMediaTime;
 begin
   FCriticalSection.Enter;
   try
-    FVolume := AVolume;
-    FMediaPlayer.Volume := FVolume;
+    Result := FDuration;
   finally
     FCriticalSection.Leave;
   end;
 end;
 
-function TSingleSound.GetDuration: TMediaTime;
+function TSingleSound.GetMediaState: TMediaState;
 begin
   FCriticalSection.Enter;
   try
-    Result := FMediaPlayer.Duration;
+    Result := FCachedMediaState;
   finally
     FCriticalSection.Leave;
   end;
 end;
 
 procedure TSingleSound.Play;
+var
+  LastCurrentTime: TMediaTime;
 begin
-  Play(FLastCurrentTime);
+  FCriticalSection.Enter;
+  try
+    LastCurrentTime := FLastCurrentTime;
+  finally
+    FCriticalSection.Leave;
+  end;
+
+  Play(LastCurrentTime);
 end;
 
 procedure TSingleSound.Play(const ACurrentTime: TMediaTime);
 var
-  CurrentTime: TMediaTime;
+  Time: TMediaTime;
+  Volume: Single;
 begin
+  RaiseMainThreadOnlyException('TSingleSound.Play');
+
+  Time := ACurrentTime;
+
   FCriticalSection.Enter;
   try
-    CurrentTime := ACurrentTime;
-    FMediaPlayer.Play;
-    FMediaPlayer.CurrentTime := CurrentTime;
-    FMediaPlayer.Volume := FVolume;
+    Volume := FVolume;
   finally
     FCriticalSection.Leave;
   end;
+
+  FSingleSoundThread.HoldEvent.SetEvent;
+  {$IFDEF ANDROID}
+  FMediaPlayer.FileName := FileName;
+  {$ENDIF}
+  FMediaPlayer.Play;
+  FMediaPlayer.CurrentTime := Time;
+  FMediaPlayer.Volume := Volume;
+
+  UpdateCache;
 end;
 
 procedure TSingleSound.Pause;
 begin
-  FCriticalSection.Enter;
-  try
-    FLastCurrentTime := FMediaPlayer.CurrentTime;
-    FMediaPlayer.Stop;
-  finally
-    FCriticalSection.Leave;
-  end;
+  RaiseMainThreadOnlyException('TSingleSound.Pause');
+
+  SaveLastValues;
+
+  FSingleSoundThread.HoldEvent.ResetEvent;
+
+  FMediaPlayer.Stop;
+
+  UpdateCache;
 end;
 
 procedure TSingleSound.Stop;
 begin
-  FCriticalSection.Enter;
-  try
-    FMediaPlayer.Stop;
-    FMediaPlayer.Clear;
-    FMediaPlayer.CurrentTime := 0;
-    FLastCurrentTime := 0;
-  finally
-    FCriticalSection.Leave;
-  end;
+  RaiseMainThreadOnlyException('TSingleSound.Stop');
+
+  ResetValues;
+
+  FMediaPlayer.Stop;
+  FMediaPlayer.Clear;
+
+  UpdateCache;
 end;
 
 procedure TSingleSound.Mute;
 begin
+  RaiseMainThreadOnlyException('TSingleSound.Mute');
+
+  SaveLastValues;
+
+  FMediaPlayer.Volume := 0;
+
+  UpdateCache;
+end;
+
+procedure TSingleSound.UnMute;
+var
+  Volume: Single;
+begin
+  RaiseMainThreadOnlyException('TSingleSound.UnMute');
+
   FCriticalSection.Enter;
   try
-    FLastVolume := FMediaPlayer.Volume;
-    Volume := 0;
+    Volume := FVolume;
+  finally
+    FCriticalSection.Leave;
+  end;
+
+  FMediaPlayer.Volume := Volume;
+
+  UpdateCache;
+end;
+
+procedure TSingleSound.SaveLastValues;
+begin
+  FCriticalSection.Enter;
+  try
+    FLastCurrentTime := FCachedCurrentTime;
   finally
     FCriticalSection.Leave;
   end;
 end;
 
-procedure TSingleSound.UnMute;
+procedure TSingleSound.OnGetDataInternalHandler;
+var
+  FileName: String;
+  CurrentTime: TMediaTime;
+  Duration: TMediaTime;
+  Volume: Single;
+  State: TMediaState;
+begin
+  UpdateCache;
+
+  FCriticalSection.Enter;
+  try
+    FileName := FFileName;
+    CurrentTime := FCachedCurrentTime;
+    Duration := FDuration;
+    Volume := FVolume;
+    State := FCachedMediaState;
+  finally
+    FCriticalSection.Leave;
+  end;
+
+  if Assigned(FOnGetDataExternalHandler) then
+    TThread.Queue(nil,
+      procedure
+      begin
+        FOnGetDataExternalHandler(
+          FileName,
+          CurrentTime,
+          Duration,
+          Volume,
+          State);
+      end);
+end;
+
+procedure TSingleSound.OnFinishedInternalHandler;
+begin
+  if Assigned(FOnFinishedExternalHandler) then
+    TThread.Queue(nil,
+      procedure
+      begin
+        FOnFinishedExternalHandler;
+      end);
+end;
+
+{ TSingleSoundThread }
+
+constructor TSingleSoundThread.Create(
+  const AThreadFactory: TThreadFactory;
+  const ASingleSound: TSingleSound);
+begin
+  FCriticalSection := TCriticalSection.Create;
+
+  FreeOnTerminate := true;
+
+  FSingleSound := ASingleSound;
+
+  FHoldEvent := TEvent.Create(nil, true, false, '', false);
+  FDoneEvent := TEvent.Create(nil, true, false, '', false);
+  FGetDataEvent := TEvent.Create(nil, true, false, '', false);
+
+  FOnGetData := nil;
+  FOnFinished := nil;
+
+  inherited Create(AThreadFactory, 'TSingleSoundThread', true);
+
+  OnSetTerminate := OnSetTerminatedHandler;
+end;
+
+destructor TSingleSoundThread.Destroy;
+begin
+  FreeAndNil(FGetDataEvent);
+  FreeAndNil(FHoldEvent);
+  FreeAndNil(FDoneEvent);
+  FreeAndNil(FCriticalSection);
+
+  inherited;
+end;
+
+procedure TSingleSoundThread.WaitForDone;
+begin
+  FDoneEvent.WaitFor(INFINITE);
+end;
+
+procedure TSingleSoundThread.SetOnGetData(const AProc: TProc);
 begin
   FCriticalSection.Enter;
   try
-    Volume := FLastVolume;
+    FOnGetData := AProc;
   finally
     FCriticalSection.Leave;
+  end;
+end;
+
+procedure TSingleSoundThread.SetOnFinished(const AProc: TProc);
+begin
+  FCriticalSection.Enter;
+  try
+    FOnFinished := AProc;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+function TSingleSoundThread.GetOnGetData: TProc;
+begin
+  FCriticalSection.Enter;
+  try
+    Result := FOnGetData;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+function TSingleSoundThread.GetOnFinished: TProc;
+begin
+  FCriticalSection.Enter;
+  try
+    Result := FOnFinished;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+procedure TSingleSoundThread.OnSetTerminatedHandler(Sender: TObject);
+begin
+  FOnGetData := nil;
+  FOnFinished := nil;
+
+  FGetDataEvent.SetEvent;
+  FHoldEvent.SetEvent;
+end;
+
+procedure TSingleSoundThread.InnerExecute;
+var
+  CurrentTime: TMediaTime;
+  Duration: TMediaTime;
+  MediaState: TMediaState;
+begin
+  FDoneEvent.ResetEvent;
+  FHoldEvent.ResetEvent;
+  FHoldEvent.WaitFor(INFINITE);
+  try
+    while not Terminated do
+    begin
+      FGetDataEvent.ResetEvent;
+
+      TThread.Queue(nil,
+        procedure
+        begin
+          if TThread.CurrentThread.ThreadID <> MainThreadID then
+            raise Exception.Create('Is not a main thread');
+
+          if Assigned(OnGetData) then
+            OnGetData();
+
+          CurrentTime := FSingleSound.CurrentTime;
+          Duration := FSingleSound.Duration;
+          MediaState := FSingleSound.MediaState;
+
+          if (CurrentTime >= Duration) and
+             (MediaState <> TMediaState.Playing)
+          then
+          begin
+            if Assigned(OnFinished) then
+            begin
+              FHoldEvent.ResetEvent;
+              OnFinished();
+            end;
+          end;
+
+          FGetDataEvent.SetEvent;
+        end);
+
+      if not Terminated then
+        FGetDataEvent.WaitFor(INFINITE);
+
+      if not Terminated then
+        FHoldEvent.WaitFor(INFINITE);
+
+      if not Terminated then
+        Sleep(100);
+    end;
+  finally
+    FDoneEvent.SetEvent;
   end;
 end;
 
